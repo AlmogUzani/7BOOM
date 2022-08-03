0176455a1a341513d1f2db0a730aa9683d83e9e3
Almog <aalmogiiii@gmail.com>
2022-08-03 10:15:52 +0300

    added getinputs in playersDetails

---
+++ src/common/playersDetails.js
@@ -0,0 +1,13 @@
+import RL from 'prompt-sync'
+const readline = RL()
+
+function getNameAndAge() {
+    const name = readline('Name: ')
+    const age = readline('Age: ')
+    return [name,age]
+}
+
+function getMaxNumber() {
+    const max = readline('Max Number: ')
+    return max
+}
